f8578f8cdd76e325ce6c3cc6537d48b5dcf5fb29
Via580 <3393896775@qq.com>
2025-06-26 23:24:35 +0800

    Java的第一个程序

---
+++ 2025_6_26/hellowword/src/hellow/HellowWord.java
@@ -0,0 +1,9 @@
+package hellow;
+
+public class HellowWord
+{
+    public static void main(String[] args)     //快捷指令：psvm＋回车
+    {
+        System.out.println("Hello Word");      //sout＋回车
+    }
+}
